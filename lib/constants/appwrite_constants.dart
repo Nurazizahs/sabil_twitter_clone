1969e1e54d46ee0fb05a2e934d1d413ae09f2e73
Nurazizahs <nurazizahsabilillah@gmail.com>
2023-06-08 22:03:14 +0700

    Saving User Data to Appwrite Database

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,7 @@
+class AppwriteConstants {
+  static const String databaseId = '6453c102c098ffd56351';
+  static const String projectId = '642a3ffc02bb21fe54cc';
+  static const String endPoint = 'https://baas.pasarjepara.com/v1';
+static const String usersCollection = '647f6df177efc81d3129';
+}
+
---
+++ lib/features/auth/controller/auth_controller.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/apis/auth_api.dart';
+import 'package:twitter_clone/core/core.dart';
+import 'package:appwrite/models.dart' as model;
+import 'package:twitter_clone/features/auth/view/login_view.dart';
+import 'package:twitter_clone/models/user_model.dart';
+import '/apis/user_api.dart';
+
+final authControllerProvider =
+  StateNotifierProvider<AuthController, bool>((ref) {
+  return AuthController(
+    authAPI: ref.watch(authAPIProvider),
+  ); 
+});
+
+final currentUserAccountProvider = FutureProvider((ref) {
+  final authController = ref.watch(authControllerProvider.notifier);
+  return authController.currentUser();
+});
+
+class AuthController extends StateNotifier<bool> {
+  final AuthAPI _authAPI;
+  AuthController({required AuthAPI authAPI})
+      : _authAPI = authAPI,
+        super(false);
+  // state = isLoading
+
+Future<model.Account?> currentUser() => _authAPI.currentUserAccount();
+
+  void signUp({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
+    state = true;
+    final res = await _authAPI.signUp(
+      email: email,
+      password: password,
+    );
+    state = false;
+    res.fold(
+      (l) => showSnackbar(context, l.message),
+      (r) async {
+        UserModel userModel = UserModel(
+          email: email,
+          name: getNameFromEmail(email),
+          followers: const [],
+          following: const [],
+          profilePic: '',
+          bannerPic: '',
+          uid: '',
+          bio: '',
+          isTwitterBlue: false,
+        );
+        final res2 = await _userAPI.saveUserData(userModel);
+        res2.fold((l) => showSnackbar(context, l.message), (r) {
+          showSnackbar(context, 'Accounted Created! Please Login');
+          Navigator.push(context, LoginView.route());
+        });
+      },
+    );
+  }
+
+  void login({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
+    state = true;
+    final res = await _authAPI.login(
+      email: email,
+      password: password,
+    );
+    state = false;
+    res.fold(
+      (l) => showSnackbar(context, l.message),
+      (r) => Navigator.push(context, HomeView.route()),
+    );
+  }
+}
