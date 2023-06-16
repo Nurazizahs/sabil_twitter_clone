56287b7b6c0392e2a770620e8f2d7b79ae708469
Nurazizahs <nurazizahsabilillah@gmail.com>
2023-06-17 02:39:19 +0700

    Picking Multiple Images & Displaying in Carousel

--- lib/features/tweet/views/create_tweet_view.dart
+++ lib/features/tweet/views/create_tweet_view.dart
@@ -1,7 +1,9 @@
  import 'package:flutter/material.dart';
  import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:twitter_clone/common/common.dart';
 import 'package:twitter_clone/common/rounded_small_button.dart';
+import 'package:twitter_clone/constants/assets_constants.dart';
 import 'package:twitter_clone/features/auth/controller/auth_controller.dart';
 import 'package:twitter_clone/theme/pallete.dart';
 
@@ -17,6 +19,12 @@ import 'package:twitter_clone/theme/pallete.dart';
 }
 
 class _CreateTweetScreenState extends ConsumerState<CreateTweetScreen> {
+  final tweetTextController = TextEditingController();
+  @override
+  void dispose(){
+    super.dispose();
+    tweetTextController.dispose();
+  }
    @override
   Widget build(BuildContext context){
     final currentUser = ref.watch(currentUserAccountProvider).value;
@@ -24,7 +32,9 @@ class _CreateTweetScreenState extends ConsumerState<CreateTweetScreen> {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          onPressed: () {},
+          onPressed: () {
+          Navigator.pop(context);
+          },
           icon: const Icon(Icons.close, size: 30),
         ),
         actions: [
@@ -42,11 +52,67 @@ class _CreateTweetScreenState extends ConsumerState<CreateTweetScreen> {
             children: [
               CircleAvatar(
                 backgroundImage: NetworkImage(currentUser.profilePic),
+                radius: 30,
+              ),
+              const SizedBox(width: 15),
+              Expanded(
+                child: TextField(
+                  controller: tweetTextController,
+                  style: const TextStyle(
+                    fontSize: 22,
+                  ),
+                  decoration: const InputDecoration(
+                  hintText: "What's happening?",
+                  hintStyle: TextStyle(
+                    color: Pallete.greyColor,
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  border: InputBorder.none, 
+                  ),
+                  maxLines: null,
+                ),
               ),
             ],
           ),
+          ),
         ),
-      )
-    );
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.only(bottom: 10),
+        decoration: const BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              color: Pallete.greyColor,
+              width: 0.3,
+            )
+          )
+        ),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0).copyWith(
+                left: 15,
+                right: 15,
+              ),
+              child: SvgPicture.asset(AssetsConstants.galleryIcon),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0).copyWith(
+                left: 15,
+                right: 15,
+              ),
+              child: SvgPicture.asset(AssetsConstants.gifIcon),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0).copyWith(
+                left: 15,
+                right: 15,
+              ),
+              child: SvgPicture.asset(AssetsConstants.emojiIcon),
+            ),
+          ],
+        ),
+      ),
+      );
   }
 }
