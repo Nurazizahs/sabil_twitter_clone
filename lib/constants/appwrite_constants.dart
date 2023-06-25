fa162991df0e99e5954181f44a56429f3ac81b2d
Nurazizahs <nurazizahsabilillah@gmail.com>
2023-06-25 22:36:40 +0700

    Displaying Tweets

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -3,11 +3,11 @@ class AppwriteConstants {
   static const String projectId = '642a3ffc02bb21fe54cc';
   static const String endPoint = 'https://baas.pasarjepara.com/v1';
   
-  static const String imagesBucket = '';
+  static const String imagesBucket = '648ff53b3987fa4b6762';
   static const String usersCollection = '647f6df177efc81d3129';
-  static const String tweetsCollection = '';
+  static const String tweetsCollection = '648ff5f2434d1ac9f919';
   // static const String usersCollection = '647f6df177efc81d3129';
 
-  static String imageUrl(String imageId)=> '';
+    static String imageUrl(String imageId) =>
+      '$endPoint/storage/buckets/$imagesBucket/files/$imageId/view?project=$projectId&mode=admin';
 }
-
